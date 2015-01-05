5b110785f4d855bea6b7081c6579b5995f04c1b3
unknown <mooringniu@mooringniu-PC2.tencent.com>
2015-01-05 22:08:53 +0800

    first build pass

--- build/make.cmd
+++ build/make.cmd
@@ -1,11 +1,11 @@
 @echo off
 set FIDDLER="D:\Programs\Fiddler2\Fiddler.exe"
 PATH=C:\Windows\Microsoft.NET\Framework\v2.0.50727;D:\Programs\NSIS\
-@del /f /q ..\obj\SmartHost.dll ..\obj\SmartHost.exe
+@del /f /q SmartHost.dll SmartHost.exe
 title Making SmartHost Plugin
-..\tools\setVersion.exe 1.1.0.3 SmartHost.cs install.nsi
+tools\setVersion.exe 1.1.0.4 ..\src\native\SmartHost.cs install.nsi
 @echo on
-@csc /o /w:1 /out:..\obj\Smarthost.dll /target:library SmartHost.cs /reference:%FIDDLER% /nologo /utf8output
+@csc /o /w:1 /out:Smarthost.dll /target:library ..\src\native\SmartHost.cs /reference:%FIDDLER% /nologo /utf8output
 @IF "%ERRORLEVEL%" NEQ "0" (
     @color f4
     @echo "Compile Dll Error"
